bf255e7dfb402207c5b539024e865b4f9cf82e1b
brandonnovy <brandonnovy@gmail.com>
2015-12-25 12:12:43 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,8 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
 makeCacheMatrix <- function(x = matrix()) {
 	# set the value of the matrix
 	# get the value of the matrix
@@ -20,4 +25,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	i <- x$getinverse()
+	# if the inverse is not blank then get it from the cache
+ 	if(!is.null(i)) {
+      	message("Getting cached data...")
+            return(i)
+        }
+
+	# if inverse is blank, then calculate the inverse
+        data <- x$get()
+        i <- solve(data, ...)
+
+	  # set the value of the inverse in the cache
+        x$setinv(i)
+        i
 }
